403d976c638d40621323562362d94c64f7249f93
Andrew-Ten-Den <andrew.ten-den@mths.ca>
2022-03-11 18:53:20 +0000

    added adding_integers

---
+++ adding_integers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Created by: Andrew-Ten-Den
+# Created on: March 11 2022
+# This program calculates the sum of two integers
+
+
+def main():
+    # This program calculates the sum of two integers
+
+    # input
+    integer1 = int(input("Enter first integer: "))
+    integer2 = int(input("Enter second integer: "))
+
+    # process
+    total = integer1 + integer2
+
+    # output
+    print("")
+    print("The total is {}.".format(total))
+    print("")
+    print("Done")
+
+
+if __name__ == "__main__":
+    main()
